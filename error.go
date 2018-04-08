12bfcdebe3a671650569fdf5182edb6ee894d872
abellocal <shlai@xtremeprog.com>
2018-04-08 11:36:05 +0800

    inital project

---
+++ error.go
@@ -0,0 +1,7 @@
+package consistenthash
+
+import (
+	"errors"
+)
+
+var NodeNotFoundErr = errors.New("node not found")
---
+++ helper.go
@@ -0,0 +1,9 @@
+package consistenthash
+
+import (
+	"hash/crc32"
+)
+
+func buildHashId(key string) uint32 {
+	return crc32.ChecksumIEEE([]byte(key))
+}
